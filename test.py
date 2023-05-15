d6ed302fd28286e58ea413134211e9335f482530
Nikita Ekert <ekertn7@gmail.com>
2023-05-15 19:28:38 +0300

    Add python scrpt.

---
+++ test.py
@@ -0,0 +1,31 @@
+import time
+import signal
+
+
+def time_break(func):
+    def wrapper(*args, **kwargs):
+        try:
+            print ("Запускаем тестируемую функцию") 
+            signal.alarm(5)
+            res = func(*args, **kwargs)
+            signal.alarm(0)
+            print ("Нормальное завершение")
+            return res
+        except Exception as e:
+            print(e)
+            return None
+
+    return wrapper
+
+
+@time_break
+def do_work():
+    j = 0;
+    while True:
+        time.sleep(1)
+        j = j + 1
+        print(j) 
+
+# Тестируем
+while True:
+    do_work()
